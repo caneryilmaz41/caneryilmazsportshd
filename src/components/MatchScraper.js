c33d27e00b89953c4196e5bae4a27a9b4f9790e6
Caner YILMAZ <beat10tr@gmail.com>
2025-10-18 23:40:29 +0300

    xc

--- src/components/MatchScraper.js
+++ src/components/MatchScraper.js
@@ -62,26 +62,15 @@ export const scrapeMatches = async () => {
 // Fallback data - TRGoals'dan alınan gerçek veriler
 export const getFallbackData = () => ({
   matches: [
-    { id: 'yayin1', name: 'Başakşehir - Galatasaray', time: '20:00' },
+    { id: 'yayin1', name: 'Fenerbahçe - Galatasaray', time: '20:00' },
     { id: 'yayinb2', name: 'Beşiktaş - Gençlerbirliği', time: '17:00' },
     { id: 'yayin1', name: 'Çaykur Rizespor - Trabzonspor', time: '17:00' },
-    { id: 'yayinbm2', name: 'Manisa FK - Erzurumspor FK', time: '13:30' },
-    { id: 'yayintrtspor', name: 'Van Spor FK - Pendikspor', time: '13:30' },
-    { id: 'yayinb3', name: 'Not.Forest - Chelsea', time: '14:30' },
-    { id: 'yayinss', name: 'Sevilla - Mallorca', time: '15:00' },
-    { id: 'yayint1', name: 'Bayern Münih - Dortmund', time: '19:30' },
-    { id: 'yayinss', name: 'Barcelona - Girona', time: '17:15' }
+ 
   ],
   channels: [
     { id: 'yayin1', name: 'BeIN Sports 1', status: '7/24' },
     { id: 'yayinb2', name: 'BeIN Sports 2', status: '7/24' },
     { id: 'yayinb3', name: 'BeIN Sports 3', status: '7/24' },
-    { id: 'yayinb4', name: 'BeIN Sports 4', status: '7/24' },
-    { id: 'yayinss', name: 'S Sport', status: '7/24' },
-    { id: 'yayinss2', name: 'S Sport 2', status: '7/24' },
-    { id: 'yayint1', name: 'Tivibu 1', status: '7/24' },
-    { id: 'yayint2', name: 'Tivibu 2', status: '7/24' },
-    { id: 'yayintrtspor', name: 'TRT Spor', status: '7/24' },
-    { id: 'yayinas', name: 'A Spor', status: '7/24' }
+ 
   ]
 })
