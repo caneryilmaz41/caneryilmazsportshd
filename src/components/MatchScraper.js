0bc5814b33acfa7f69b3015a98519e375a4116d2
Caner YILMAZ <beat10tr@gmail.com>
2025-10-18 19:56:48 +0300

    fdgd

--- src/components/MatchScraper.js
+++ src/components/MatchScraper.js
@@ -1,6 +1,5 @@
-// Aktif domain listesi - 1430-1445 aralığı
+// Aktif domain listesi - 1431'den başlatıp güncel olanlar
 const TRGOALS_DOMAINS = [
-  'https://trgoals1430.xyz',
   'https://trgoals1431.xyz',
   'https://trgoals1432.xyz',
   'https://trgoals1433.xyz',
@@ -15,7 +14,8 @@ const TRGOALS_DOMAINS = [
   'https://trgoals1442.xyz',
   'https://trgoals1443.xyz',
   'https://trgoals1444.xyz',
-  'https://trgoals1445.xyz'
+  'https://trgoals1445.xyz',
+  'https://trgoals1446.xyz'
 ]
 
 // TRGoals'dan maç listesini çek
@@ -59,18 +59,29 @@ export const scrapeMatches = async () => {
   return getFallbackData()
 }
 
-// Fallback data
+// Fallback data - TRGoals'dan alınan gerçek veriler
 export const getFallbackData = () => ({
   matches: [
-    { id: 'yayin1', name: 'Galatasaray - Fenerbahçe', time: '20:00' },
-    { id: 'yayinb2', name: 'Real Madrid - Barcelona ', time: '20:00' },
-    { id: 'yayint3', name: 'Kocaelispor - Beşiktaş', time: '19:30' },
-    { id: 'yayint3', name: 'Vanspor - Erzurumspor', time: '19:30' }
-      
+    { id: 'yayin1', name: 'Başakşehir - Galatasaray', time: '20:00' },
+    { id: 'yayinb2', name: 'Beşiktaş - Gençlerbirliği', time: '17:00' },
+    { id: 'yayin1', name: 'Çaykur Rizespor - Trabzonspor', time: '17:00' },
+    { id: 'yayinbm2', name: 'Manisa FK - Erzurumspor FK', time: '13:30' },
+    { id: 'yayintrtspor', name: 'Van Spor FK - Pendikspor', time: '13:30' },
+    { id: 'yayinb3', name: 'Not.Forest - Chelsea', time: '14:30' },
+    { id: 'yayinss', name: 'Sevilla - Mallorca', time: '15:00' },
+    { id: 'yayint1', name: 'Bayern Münih - Dortmund', time: '19:30' },
+    { id: 'yayinss', name: 'Barcelona - Girona', time: '17:15' }
   ],
   channels: [
     { id: 'yayin1', name: 'BeIN Sports 1', status: '7/24' },
     { id: 'yayinb2', name: 'BeIN Sports 2', status: '7/24' },
-    { id: 'yayinss', name: 'S Sport', status: '7/24' }
+    { id: 'yayinb3', name: 'BeIN Sports 3', status: '7/24' },
+    { id: 'yayinb4', name: 'BeIN Sports 4', status: '7/24' },
+    { id: 'yayinss', name: 'S Sport', status: '7/24' },
+    { id: 'yayinss2', name: 'S Sport 2', status: '7/24' },
+    { id: 'yayint1', name: 'Tivibu 1', status: '7/24' },
+    { id: 'yayint2', name: 'Tivibu 2', status: '7/24' },
+    { id: 'yayintrtspor', name: 'TRT Spor', status: '7/24' },
+    { id: 'yayinas', name: 'A Spor', status: '7/24' }
   ]
 })
