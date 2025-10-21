2c9b5f256e72e21ddb2d909e7533ce06d0efb16d
Caner YILMAZ <beat10tr@gmail.com>
2025-10-21 23:02:43 +0300

    za

--- src/components/StreamService.js
+++ src/components/StreamService.js
@@ -22,8 +22,7 @@ const TRGOALS_DOMAINS = [
 export const getStreamUrl = async (channelId) => {
   // Arsenal maçı için özel URL
   if (channelId === 'arsenal_atletico') {
-    const hlsUrl = encodeURIComponent('https://hlssssssss.puanicinumut.workers.dev/https://corestream.ronaldovurdu.help//hls/tabii.m3u8')
-    return `https://www.hlsplayer.net/player/?url=${hlsUrl}`
+    return 'https://m3u8player.org/player.html?url=https://hlssssssss.puanicinumut.workers.dev/https://corestream.ronaldovurdu.help//hls/tabii.m3u8'
   }
   
   return `${TRGOALS_DOMAINS[0]}/channel.html?id=${channelId}`
